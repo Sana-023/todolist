41d094f407944e6933ea048a6c2b19bb901b34f5
Snoo <snoo6586@gmail.com>
2024-07-06 19:26:41 +0300

    header component

---
+++ src/lib/Header.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import dayjs from 'dayjs'
+
+	let period = dayjs().format('a')=='pm'? 'Evening':'morning';
+</script>
+
+<header class ="flex justify-between">
+    <div>
+        <h1 class="text-4xl mb-2">
+            Good {period}
+        </h1>
+        <h2 class="text-surface-600/50">
+            {dayjs().format(' dddd D MMMM YYYY')}
+        </h2>
+    </div>
+    <div class="bg-white w-32 h-14"></div>
+</header>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,9 @@
 <script lang="ts">
-	import dayjs from 'dayjs'
-
-	let period = dayjs().format('a')=='pm'? 'evening':'morning';
+	import Header from "$lib/Header.svelte";
 </script>
 
-
 <div class="p-16">
-	<header class ="flex justify-between">
-		<div>
-			<h1 class="text-4xl mb-2">
-				Good {period}
-			</h1>
-			<h2 class="text-surface-600/50">
-				{dayjs().format(' dddd D MMMM YYYY')}
-			</h2>
-		</div>
-		<div class="bg-white w-32 h-14"></div>
-	</header>
+	<Header/>
+	
 </div>
 
