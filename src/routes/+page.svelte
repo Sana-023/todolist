0a7e2a73c4e5605c62b8ee4fcc31afc847e7678d
Snoo <snoo6586@gmail.com>
2024-07-06 19:20:00 +0300

    morning and evening

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,21 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+<script lang="ts">
+	import dayjs from 'dayjs'
 
-<div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-5">
-		<h1 class="h1">Let's get cracking bones!</h1>
-		<p>Start by exploring:</p>
-		<ul>
-			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout</li>
-			<li><code class="code">/src/app.postcss</code> - app wide css</li>
-			<li>
-				<code class="code">/src/routes/+page.svelte</code> - this page, you can replace the contents
-			</li>
-		</ul>
-	</div>
+	let period = dayjs().format('a')=='pm'? 'evening':'morning';
+</script>
+
+
+<div class="p-16">
+	<header class ="flex justify-between">
+		<div>
+			<h1 class="text-4xl mb-2">
+				Good {period}
+			</h1>
+			<h2 class="text-surface-600/50">
+				{dayjs().format(' dddd D MMMM YYYY')}
+			</h2>
+		</div>
+		<div class="bg-white w-32 h-14"></div>
+	</header>
 </div>
+
