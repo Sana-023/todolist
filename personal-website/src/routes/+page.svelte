1408ae8a2e66afa60a1e7993d5903357cac740e5
Snoo <snoo6586@gmail.com>
2024-07-20 01:33:25 +0300

    color

--- personal-website/src/routes/+page.svelte
+++ personal-website/src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import ThemeSwitch from "$lib/components/ThemeSwitch.svelte";
+import Button from "$lib/components/ui/button/button.svelte";
+
+</script>
+
+<div class="flex justify-center items-center min-h-screen">
+ hello word
+    <ThemeSwitch />
+  
+</div>
