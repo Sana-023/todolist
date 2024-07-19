8c4a2faecf1f9fa134faf6b59c21b833573e4c93
Snoo <snoo6586@gmail.com>
2024-07-20 01:51:14 +0300

    pages

--- personal-website/src/routes/+page.svelte
+++ personal-website/src/routes/+page.svelte
@@ -1,11 +1,9 @@
-<script>
-    import ThemeSwitch from "$lib/components/ThemeSwitch.svelte";
-import Button from "$lib/components/ui/button/button.svelte";
+<script lang='ts'>
+    import Button from "$lib/components/ui/button/button.svelte";
 
 </script>
 
-<div class="flex justify-center items-center min-h-screen">
- hello word
-    <ThemeSwitch />
-  
-</div>
+<Button href="/contact" variant="link" >
+    Contact
+    
+    </Button>
