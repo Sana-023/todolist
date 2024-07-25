6883b9749ac53e69b5fd6ac3360a65754361ec48
Snoo <snoo6586@gmail.com>
2024-07-25 06:48:11 +0300

    education card

--- personal-website/src/lib/components/Footer.svelte
+++ personal-website/src/lib/components/Footer.svelte
@@ -3,21 +3,21 @@
 
 </script>
 <footer class=" bg-secondary text-secondary-foreground py-2 px-6">
-    <div class=" max-w-screen-2xl flex justify-between items-center mx-auto ">
-        <span>
+    <div class=" max-w-screen-2xl flex flex-row-reverse md:flex-row justify-between items-center mx-auto ">
+        <span class=" self-end md:self-center">
             © {(new Date()).getFullYear()} 
         </span>
 
-       <div class="flex gap-16">
-        <span class="flex flex-col items-center">
+       <div class="flex flex-col md:flex-row gap-4  md:gap-16">
+        <span class="flex gap-4 md:gap-0 md:flex-col items-center">
             <span class="font-bold">Call</span> 
             <a href="tel:0599916010">0599916010</a> 
          </span>
-         <span class="flex flex-col items-center">
-             <span class="font-bold">Email</span> 
+         <span class="flex gap-4 md:gap-0 md:flex-col items-center">
+             <span class="font-bold ">Email</span> 
              <a href="mailto:snoo6586@gmail.com">snoo6586@gmail.com</a> 
           </span>
-          <span class="flex items-end " >            
+          <span class="flex items-end hidden md:flex " >            
              <SocialMedia />
           </span>
        </div>
