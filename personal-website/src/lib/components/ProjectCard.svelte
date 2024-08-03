5a4a0bbf4d4001b083c89e22d718b9caf9d54fb6
Snoo <snoo6586@gmail.com>
2024-08-04 02:38:54 +0300

    edit project

---
+++ personal-website/src/lib/components/ProjectCard.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+
+    
+    export let title: string;
+    export let subtitle: string;
+    export let imageSrc: string;
+
+     
+ 
+ </script>
+ 
+ 
+ 
+ <div class="bg-secondary text-secondary-foreground  shadow-xl flex flex-col sm:flex-row gap-4 justify-between" >
+    <div class="flex flex-col gap-16 pt-6  ">
+        <div class="flex flex-col min-w-32 border-s-8 border-primary ps-6 "> 
+            <span class=" font-bold text-primary text-xl">  {title} </span>
+            <span class=" font-light-semibold ">  {subtitle} </span>
+           
+    
+        </div>
+        <div class="ps-6 pb-8"> 
+            <slot />
+   
+        </div>
+    
+    </div>
+    <img src={imageSrc} alt="project image" class="w-auto sm:w-64 object-cover" />
+    </div>
+
+    
--- personal-website/src/routes/projects/+page.svelte
+++ personal-website/src/routes/projects/+page.svelte
@@ -1,9 +1,53 @@
 <script>
     import Container from "$lib/components/Container.svelte";
     import Title from "$lib/components/Title.svelte";
+    import ProjectCard from "$lib/components/ProjectCard.svelte";
+     
 
 </script>
-<Container >
+<Container class="flex flex-col gap-16" >
     <Title title="Projects" />
+   
+
+    <ProjectCard 
+    title="Calculator "
+     subtitle="Calculating simple equations" 
+     imageSrc= "/project1.png">
+    
+     <ul class="list-disc ps-2 ">
+        <li>
+            Clear and simple design: Displays numbers and results in a clear and easy-to-read manner.
+        </li>
+        <li>
+            The ability to perform basic operations such as addition, subtraction, multiplication, and division.
+
+        </li>
+        
+        <li>
+            No Complexity: Designed only for simple calculations. 
+        </li>
+     </ul>
+    </ProjectCard>
+
+    <ProjectCard 
+    title="To-Do List "
+     subtitle="to write down daily tasks" 
+     imageSrc= "/project2.png">
+    
+     <ul class="list-disc ps-2 ">
+        <li>
+            Task Addition: Provides a field for entering tasks and their details, such as titles, descriptions, and due dates.
+        </li>
+        <li>
+            Task Editing and Deletion: Allows users to modify details or delete unnecessary or completed tasks.
+
+        </li>
+        
+     </ul>
+    </ProjectCard>
+
 
 </Container>
+
+
+
